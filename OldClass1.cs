d47b452e74fb8eaea4ab3d3254d87b63f2b911b2
Joseph Woodward <joseph.woodward@xeuse.com>
2017-10-04 14:03:23 +0100

    Initial commit

---
+++ OldClass1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using EventStore.ClientAPI;
+
+/*namespace EventStoreSandbox
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+                    var connection =
+                EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113));
+
+            // Don't forget to tell the connection to connect!
+            connection.ConnectAsync().Wait();
+
+            var myEvent = new EventData(Guid.NewGuid(), "JoesEvent", false, Encoding.UTF8.GetBytes("Hello world data"), Encoding.UTF8.GetBytes("Hello World Meta Data!"));
+
+            connection.AppendToStreamAsync("joes-stream", ExpectedVersion.Any, myEvent).Wait();
+
+            var streamEvents = connection.ReadStreamEventsForwardAsync("joes-stream", 0, 1, false).Result;
+
+            var returnedEvent = streamEvents.Events[0].Event;
+            Console.WriteLine("Read event with data: {0}, metadata: {1}", Encoding.UTF8.GetString(returnedEvent.Data), Encoding.UTF8.GetString(returnedEvent.Metadata));
+
+
+
+
+            var connection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113));
+
+            // Don't forget to tell the connection to connect!
+            connection.ConnectAsync().Wait();
+
+            var myEvent = new EventData(Guid.NewGuid(), "JoesEvent", false, Encoding.UTF8.GetBytes("Hello world data"),
+                Encoding.UTF8.GetBytes("Hello World Meta Data!"));
+
+            connection.AppendToStreamAsync("joes-stream", ExpectedVersion.Any, myEvent).Wait();
+            StreamEventsSlice streamEvents;
+            do
+            {
+                streamEvents = connection.ReadStreamEventsForwardAsync("joes-stream", 0, 1, false).Result;
+                foreach (var @event in streamEvents.Events)
+                {
+                    var res = @event.Event.Data;
+                    Console.WriteLine("Read event with data: {0}, metadata: {1}", Encoding.UTF8.GetString(res));
+                }
+                
+            } while (!streamEvents.IsEndOfStream);
+            
+            Console.ReadLine();
+        }
+    }
+}*/
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using EventStore.ClientAPI;
+
+namespace EventStoreSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113));
+
+            // Don't forget to tell the connection to connect!
+            connection.ConnectAsync().Wait();
+            var myEvent = new EventData(Guid.NewGuid(), "JoesEvent", false, Encoding.UTF8.GetBytes("Hello world data"), Encoding.UTF8.GetBytes("Hello World Meta Data!"));
+
+            connection.AppendToStreamAsync("joes-stream", ExpectedVersion.Any, myEvent).Wait();
+
+            StreamEventsSlice streamEvents;
+            do
+            {
+                streamEvents = connection.ReadStreamEventsForwardAsync("joes-stream", 0, 1, false).Result;
+                foreach (var @event in streamEvents.Events)
+                {
+                    var res = @event.Event.Data;
+                    Console.WriteLine("Read event with data: {0}, metadata: {1}", Encoding.UTF8.GetString(res), Encoding.UTF8.GetString(@event.Event.Metadata));
+                }
+                
+            } while (!streamEvents.IsEndOfStream);
+            Console.ReadLine();
+        }
+    }
+}
